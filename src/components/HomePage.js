ab1fd2c9631c072db5a5effc79fa310e557a7938
Lee <justin00@ead.ubc.ca>
2016-12-15 13:56:35 -0800

    set up CLF

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome, First-Name Last-Name</h1>
+
+        </div>
+    );
+};
+
+export default HomePage;

f84c50b78e47bb52b0331f8fc8345836981e7339
Lee <justin00@ead.ubc.ca>
2016-12-15 13:23:32 -0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {render} from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import configureStore from './store/configureStore';
+
+const store = configureStore();

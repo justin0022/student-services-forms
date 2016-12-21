9c6c31ea0e70c9835d5f7a6eab3d521ada738a37
Justin Lee <justin.lee@ubc.ca>
2016-12-21 15:02:40 -0800

    ADDED: type checking for NameField

--- src/components/NameField.js
+++ src/components/NameField.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import {Field} from 'redux-form';
 import * as validate from '../constants/Validate';
 import RenderField from './RenderField';
@@ -6,5 +6,11 @@ import RenderField from './RenderField';
 const NameField = (props) => (
     <Field name={props.name} type="text" component={RenderField} label={props.label} placeholder={props.placeholder} validate={[ validate.required ]}/>
 );
-        
+
+NameField.propTypes = {
+    name: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    placeholder: PropTypes.string.isRequired,
+};
+
 export default NameField;

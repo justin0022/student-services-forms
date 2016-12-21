e068d9d9c83f5e4772540849708666416a14ca78
Justin Lee <justin.lee@ubc.ca>
2016-12-21 11:57:56 -0800

    ADDED: client-side field validation

--- src/components/AcademicConsessionFormView.js
+++ src/components/AcademicConsessionFormView.js
@@ -1,58 +1,86 @@
-import React from 'react';
-import {Field, reduxForm} from 'redux-form';
+import React from 'react'
+import {Field, reduxForm} from 'redux-form'
+import RenderField from './RenderField'
+import * as validate from '../constants/Validate'
 
-
-class AcademicConsessionFormView extends React.Component {
-    render() {
-        const { handleSubmit } = this.props;
-        return (
-            <form onSubmit={handleSubmit}>
-                <h1>Request for Academic Consession</h1>
-                <div className="row-fluid">
-                    <div className="span6">
-                        <label htmlFor="firstName">First Name</label>
-                        <Field name="firstName" component="input" type="text" placeholder="First Name" className="input"/>
-                    </div>
-                    <div className="span6">
-                        <label htmlFor="lastName">Last Name</label>
-                        <Field name="lastName" component="input" type="text" placeholder="Last Name" className="input"/>
+const AcademicConsessionFormView = (props) => {
+    const { handleSubmit } = props;
+    return (
+        <form onSubmit={handleSubmit}>
+            <h1>Request for Academic Consession</h1>
+            <div className="row-fluid">
+                <div className="span6">
+                    <Field name="firstName" type="text" component={RenderField} label="First Name"/>
+                    <Field name="username" type="text"
+                        component={RenderField} label="Username"
+                        validate={[ validate.required, validate.maxLength15 ]}
+                    />
+                </div>
+                <div className="span6">
+                    <label htmlFor="lastName">Last Name</label>
+                    <Field name="lastName" component="input" type="text" placeholder="Last Name" className="input"/>
+                </div>
+            </div>
+            <div className="row-fluid">
+                <div className="span6">
+                    <label htmlFor="studentNumber">Student Number</label>
+                    <Field name="studentNumber" component="input" type="text" placeholder="Student Number" className="input"/>
+                </div>
+            </div>
+            <div className="row-fluid">
+                <div className="span6">
+                    <label htmlFor="email">Email</label>
+                    <Field name="email" component="input" type="text" placeholder="Email" className="input"/>
+                </div>
+            </div>
+            <div className="row-fluid">
+                <div className="span6">
+                    <label htmlFor="year">Year Level</label>
+                    <div>
+                        <Field name="yearLevel" component="select" className="input">
+                            <option></option>
+                            <option value="Year 1">Year 1</option>
+                            <option value="Year 2">Year 2</option>
+                            <option value="Year 3">Year 3</option>
+                            <option value="Year 4">Year 4</option>
+                        </Field>
                     </div>
                 </div>
-                <div className="row-fluid">
-                    <div className="span6">
-                        <label htmlFor="studentNumber">Student Number</label>
-                        <Field name="studentNumber" component="input" type="text" placeholder="Student Number" className="input"/>
+            </div>
+            <div className="row-fluid">
+                <div className="span6">
+                    <label htmlFor="department">Department/Program (if applicable)</label>
+                    <div>
+                        <Field name="department" component="select" className="input">
+                            <option></option>
+                            <option value="APBI">APBI</option>
+                            <option value="FNH">FNH</option>
+                            <option value="GRS">GRS</option>
+                        </Field>
                     </div>
                 </div>
-                <div className="row-fluid">
-                    <div className="span6">
-                        <label htmlFor="email">Email</label>
-                        <Field name="email" component="input" type="text" placeholder="Email" className="input"/>
+            </div>
+            <div className="row-fluid">
+                <div className="span6">
+                    <label htmlFor="numberOfCourses">For how many courses do you need consession?</label>
+                    <div>
+                        <Field name="numberOfCourses" component="select" className="input">
+                            <option></option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </Field>
                     </div>
                 </div>
-                <div className="row-fluid">
-                    <div className="span6">
-                        <label htmlFor="year">Year Level</label>
-                        <div>
-                            <Field name="yearLevel" component={select}>
-                                <option></option>
-                                <option value="Year 1">Year 1</option>
-                                <option value="Year 2">Year 2</option>
-                                <option value="Year 3">Year 3</option>
-                                <option value="Year 4">Year 4</option>
-                            </Field>
-                        </div>
-                    </div>
-                </div>
-            </form>
-            
-        );
-    }
+            </div>
+        </form>
+        
+    )
 }
 
-AcademicConsessionFormView = reduxForm({
+export default reduxForm({
     form: 'AcademicConsessionFormView',
     destroyOnUnmount: false
 })(AcademicConsessionFormView);
-
-export default AcademicConsessionFormView;

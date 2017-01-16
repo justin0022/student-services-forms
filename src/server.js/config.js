189f7410d81145b63259e295fc3d678f8b608c7e
Justin Lee <justin.lee@ubc.ca>
2017-01-16 12:42:52 -0800

    ADDED: more server configs

---
+++ src/server.js/config.js
@@ -0,0 +1,9 @@
+const config = {
+    user: 'justin', 
+    database: 'justin',
+    password: '',
+    host: 'localhost',
+    port: '5432',
+};
+
+export default config;
---
+++ src/server.js/pghelper.js
@@ -0,0 +1,11 @@
+import pg from 'pg';
+import config from './config';
+
+let pool = new pg.Pool(config);
+
+pool.connect((err, client, done) => {
+    if (err) {
+        return console.error('error fetching client from pool', err);
+    }
+    client.query()
+});
